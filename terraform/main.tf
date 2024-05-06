68b2d945bf6117baa249aff5db94f1c0a150f90c
cgradwohl <15326707+cgradwohl@users.noreply.github.com>
2024-05-05 17:31:38 -0700

    remove profile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-
 provider "aws" {
-  region  = "us-west-1"
-  profile = "default"
+  region = "us-west-1"
 }
 
 # ECR repository
